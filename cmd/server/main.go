8fee003adfd713f779cae510be5a04e4474bb680
Fer Rios <fer.rios.costa@gmail.com>
2021-09-11 17:13:21 +0000

    App struct created

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
+// App - the struct which contains things like pointers to database connections
+type App struct{}
 
+func (app *App) Run() error {
+	fmt.Println("Setting Up our App.")
+	return nil
+}
+
+func main() {
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error Starting Up.")
+		fmt.Println(err)
+	}
 	fmt.Println("Go server")
 }
