aa33fc2189a718842676446e941a1fbcd217fdc4
Fer Rios <fer.rios.costa@gmail.com>
2021-09-13 21:53:08 +0000

    Docker compose and test added

---
+++ test/comment_test.go
@@ -0,0 +1,37 @@
+//go:build e2e
+// +build e2e
+
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetComments(t *testing.T) {
+	fmt.Println("Running E2E test fot get comments endpoint")
+
+	client := resty.New()
+	resp, err := client.R().Get(BASE_URL + "/api/comment/")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestPostComments(t *testing.T) {
+	fmt.Println("Running E2E test fot POST comments endpoint")
+
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(`{"slug": "/test", "author": "test", "body": "hello world"}`).
+		Post(BASE_URL + "/api/comment/")
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
---
+++ test/health_test.go
@@ -0,0 +1,24 @@
+//go:build e2e
+// +build e2e
+
+package test
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHealthEndpoint(t *testing.T) {
+	fmt.Println("Running E2E test fot health check endpoint")
+
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8080/api/health")
+	if err != nil {
+		fmt.Println(resp.StatusCode())
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
